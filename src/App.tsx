5f46fb2d2831f711ec3564a77c6e266f8c2c088d
Joshua Schoonover <joshua.schoonover@austincc.edu>
2024-07-08 22:27:33 -0400

    Card body styling

--- src/App.tsx
+++ src/App.tsx
@@ -54,38 +54,46 @@ function App() {
 
   return (
     <div className="App">
+      <div className="Card">
+
+
 
       <h1>Scutes go here</h1>
 
-      <p>A quick app to help calculate the Scutes that would be made from dropping land in MTG.</p>
-      <p>Landfall — Whenever a land enters the battlefield under your control, create a 1/1 green Insect creature token. If you control six or more lands, create a token that's a copy of Scute Swarm instead.</p>
-      
-      <hr/>
-      <div>current land on field</div>
-      <NumBox value={currentLands} onChange={adjustCurrentLands}/>
-      <div>is land on field 6 or over? disable above box</div>
-      <div>todo: if the lands come from one spell then they don't cascade the same</div>
-      <div>how many land are being added?</div>
-      <NumBox value={landsHittingTheField} onChange={adjustLandsHittingTheField}/>
-      <div>go button</div>
-      <input type='button' value='CALCULATE!' onClick={calculateHandler}/>
-      <hr/>
-      <div>current scutes</div>
-      <NumBox value={initialScutes} onChange={adjustInitialScutes}/>
-      <div>new scutes</div>
-      <NumBox value={extraScutes} readonly/>
-      <div>how many buggies if under 6 land</div>
-      <NumBox value={buggers} readonly/>
+      <div className="BodyText">
+        <p>A quick app to help calculate the Scutes that would be made from dropping land in MTG.</p>
+        <p>Landfall — Whenever a land enters the battlefield under your control, create a 1/1 green Insect creature token. If you control six or more lands, create a token that's a copy of Scute Swarm instead.</p>
+      </div>
 
-      <hr/>
-      <div>roll to next turn</div>
-      <input type='button' value='Next Turn' onClick={nextTurn}/>
-      <div>todo: find a way to kill some</div>
+      <div className="BodyText">
+        <div>current land on field</div>
+        <NumBox value={currentLands} onChange={adjustCurrentLands}/>
+        {/* <div>is land on field 6 or over? disable above box</div> */}
+        <div>how many land are being added?</div>
+        <NumBox value={landsHittingTheField} onChange={adjustLandsHittingTheField}/>
+        <div>go button</div>
+        <input type='button' value='CALCULATE!' onClick={calculateHandler}/>
+      </div>
+      <div className="BodyText">
+        <div>current scutes</div>
+        <NumBox value={initialScutes} onChange={adjustInitialScutes}/>
+        <div>new scutes</div>
+        <NumBox value={extraScutes} readonly/>
+        <div>how many buggies if under 6 land</div>
+        <NumBox value={buggers} readonly/>
+      </div>
+      <div className="BodyText">
+        <div>Roll new scutes into current scutes. This represents "end of turn" or similiar that will combine the states of the cards.</div>
+        <input type='button' value='Next Turn' onClick={nextTurn}/>
+        {/* <div>todo: find a way to kill some</div> */}
+      </div>
 
 
 
-      <hr/>
-      <p>Disclaimer: I own nothing of WOTC or MTG witch are big things, please don't take me down.</p>
+
+      </div>
+      <p>Disclaimer: I own nothing of WOTC or MTG which are big things, please don't take me down.</p>
+
     </div>
   );
 }
